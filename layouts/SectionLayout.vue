aea0d0c61c7b944e6f2f94cb4ad1095b0955ca06
dimasgda <dimasgilang0803@gmail.com>
2025-04-09 16:12:57 +0700

    feat: create pricing section

---
+++ layouts/SectionLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <section :class="`${bgColor} w-full`">
+    <div class="px-4 py-12 mx-auto max-w-7xl">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  bgColor: String
+})
+</script>
+
+<style></style>
