4d9ed25379e4bffd238ae23e9430e57410913e56
Ranu Dewantara <133847667+RanuDewantara@users.noreply.github.com>
2025-04-09 15:54:33 +0700

    feat: navbar and footer (#10)
    Co-authored-by: GlowUp Career Consultant <developer.gucc@gmail.com>

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="relative bottom-0 bg-gu-navy text-gu-white py-3 items-center gap-1 text-center">
+    <span class="px-2">©</span>
+    <span class="text-sm">{{ year }} {{ name }}</span>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+const year = new Date().getFullYear()
+const name = 'GlowUp Career Consultant'
+</script>
+
+<style>
+
+</style>
--- components/Welcome.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="font-sans min-h-dvh w-full flex items-center justify-center text-3xl font-bold">Under Construction...🧑‍💻</div>
-</template>
-
-<script lang="ts" setup>
-
-</script>
