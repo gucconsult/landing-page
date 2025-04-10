b48fc5763d75b4b4ce86b8178648631864df152b
RanuDewantara <ranuputradewantara@programmer.net>
2025-04-10 16:38:18 +0700

    fix: feature section and main.css

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="max-w-7xl mx-auto text-dark space-y-14">
-    <div class="mx-auto px-4">
+    <!-- <div class="mx-auto px-4 text-center md:text-justify">
       <h1 class="font-semibold text-2xl">Kami akan menemani perjalanan sukses anda</h1>
       <h2 class="text-base">Di sini anda akan mendapatkan layanan berupa</h2>
-    </div>
+    </div> -->
 
+    <UiHeading title="Kami akan menemani perjalanan sukses anda" subtitle="Di sini anda akan mendapatkan layanan berupa"" />
     <div class="grid grid-cols-1 text-justify justify-between mb-20 md:grid-cols-3">
-      <div class=" px-4 text-base max-w-sm">
+      <div class="px-5 py-5 text-base max-w-sm">
         <img src="../assets/images/career.svg" alt="career" class="h-12 mb-5">
         <h1 class="text-xl py-1"><b>Bimbingan Karier</b></h1>
         <p class="text-base">
           Bimbingan karier untuk menjadi seorang profesional dalam berkarier sesuai bidang.
         </p>
       </div>
-      <div class="px-4 text-base max-w-sm">
+      <div class="px-5 py-5 text-base max-w-sm">
         <img src="../assets/images/team.svg" alt="team" class="h-12 mb-5">
         <h1 class="text-xl py-1"><b>Tim Berpengalaman</b></h1>
         <p class="text-base">
           Anda akan dibimbing oleh tim yang berpengalaman di bidang karier profesional
         </p>
       </div>
-      <div class="px-4 text-base max-w-sm">
+      <div class="px-5 py-5 text-base max-w-sm">
         <img src="../assets/images/cheap.svg" alt="cheap" class="h-12 mb-5">
         <h1 class="text-xl py-1"><b>Biaya Terjangkau</b></h1>
         <p class="text-base">
