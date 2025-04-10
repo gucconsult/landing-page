b3ad4e5b35d60adef34de180c589ca139ade4540
dimasgda <dimasgilang0803@gmail.com>
2025-04-10 13:54:56 +0700

    style: reduce black color to gray 900, and fix some margin

--- components/ui/Heading.vue
+++ components/ui/Heading.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="space-y-2 text-center sm:text-start">
-    <h2 class="text-2xl font-bold text-white">{{ title }}</h2>
-    <span class="text-sm font-normal text-white">{{ subtitle }}</span>
+  <div :class="`space-y-2 text-center ${positionClass[position]}`">
+    <h2 :class="`text-2xl font-bold ${color[variant]}`">{{ title }}</h2>
+    <span :class="`text-sm font-normal ${color[variant]}`">{{ subtitle }}</span>
   </div>
 </template>
 
 <script lang="ts" setup>
 defineProps({
   title: String,
-  subtitle: String
+  subtitle: String,
+  variant: {
+    type: String,
+    default: 'default'
+  },
+  position: {
+    type: String,
+    default: 'start'
+  }
 })
+
+const color = {
+  default: 'text-gray-900',
+  white: 'text-white'
+}
+
+const positionClass = {
+  start: 'sm:text-start',
+  center: 'sm:text-center',
+}
+
 </script>
--- components/ui/PricingCard.vue
+++ components/ui/PricingCard.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex flex-col items-center max-w-sm gap-3 px-5 py-6 bg-white rounded-sm">
-        <span class="text-2xl font-extrabold">{{ title }}</span>
-        <span class="text-3xl font-black text-black">{{ price }}</span>
+        <span class="text-2xl font-extrabold text-gray-900">{{ title }}</span>
+        <span class="text-3xl font-black text-gray-900">{{ price }}</span>
         <a href="#" class="block w-full px-3 py-2 font-bold text-center text-white rounded-sm bg-secondary">
             Pesan Sekarang
         </a>
         <ul class="w-full space-y-2">
             <li v-for="benefit in benefits" :key="benefit.id" class="flex gap-1">
                 <Check />
-                <span class="text-sm">{{ benefit }}</span>
+                <span class="text-sm text-gray-900">{{ benefit }}</span>
             </li>
         </ul>
         <button class="text-sm font-semibold cursor-pointer text-secondary" v-if="isShowMore"
