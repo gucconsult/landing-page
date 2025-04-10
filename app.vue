5bac472ea7f85ab72183fd0c0270bbfa1bce5e7e
Dimas Gilang Dwi Aji <dimasgilang0803@gmail.com>
2025-04-10 09:44:03 +0700

    feat: add meta tag, opengraph, and twitter (#13)
    * feat: add meta tag, opengraph, and twitter

--- app.vue
+++ app.vue
@@ -1,5 +1,33 @@
 <template>
   <NuxtLayout>
-    <NuxtPage class="font-sans"/>
+    <NuxtPage class="font-sans scroll-smooth antialiased" />
   </NuxtLayout>
 </template>
+
+<script lang="ts" setup>
+useHead({
+  htmlAttrs: {
+    lang: 'id'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.ico'
+    }
+  ]
+})
+
+useSeoMeta({
+  title: 'GlowUp Career Consultant',
+  description: 'GlowUp Career Consultant adalah start-up pengembangan karir yang menawarkan layanan lengkap, termasuk pembuatan/review CV, optimasi portofolio, LinkedIn, pelatihan interview, serta career coaching',
+  ogTitle: 'GlowUp Career Consultant',
+  ogDescription: 'GlowUp Career Consultant adalah start-up pengembangan karir yang menawarkan layanan lengkap, termasuk pembuatan/review CV, optimasi portofolio, LinkedIn, pelatihan interview, serta career coaching',
+  ogImage: '/og-image.webp',
+  ogUrl: 'https://glowupcareerconsultant.vercel.app',
+  twitterTitle: 'GlowUp Career Consultant',
+  twitterDescription: 'GlowUp Career Consultant adalah start-up pengembangan karir yang menawarkan layanan lengkap, termasuk pembuatan/review CV, optimasi portofolio, LinkedIn, pelatihan interview, serta career coaching',
+  twitterImage: '/og-image.webp',
+  twitterCard: 'summary'
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,59 +10,6 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
 
-  app: {
-    head: {
-      title: "GlowUp Carrer Consultant",
-      htmlAttrs: {
-        lang: "id",
-      },
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        {
-          name: "description",
-          content: "GlowUp Carrer Consultant",
-        },
-        {
-          name: "keywords",
-          content: "GlowUp Carrer Consultant",
-        },
-        {
-          name: "author",
-          content: "GlowUp Carrer Consultant",
-        },
-        {
-          property: "og:title",
-          content: "GlowUp Carrer Consultant",
-        },
-        {
-          property: "og:description",
-          content: "GlowUp Carrer Consultant",
-        },
-        /**
-         * TODO : Add Open Graph Image and Twitter Card
-         */
-        // {
-        //   property: "og:image",
-        //   content: "https://example.com/og-image.jpg",
-        // },
-        // {
-        //   property: "twitter:title",
-        //   content: "GlowUp Carrer Consultant",
-        // },
-        // {
-        //   property: "twitter:description",
-        //   content: "GlowUp Carrer Consultant",
-        // },
-        // {
-        //   property: "twitter:image",
-        //   content: "https://example.com/og-image.jpg",
-        // },
-      ],
-    },
-  },
-
   modules: ["@nuxt/fonts"],
   fonts: {
     defaults: {
