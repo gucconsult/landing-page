77622288ee047c0fcb298bcfd5932a87cd6381fe
dimasgda <dimasgilang0803@gmail.com>
2025-04-19 12:42:58 +0700

    fix(meta): change description

--- app.vue
+++ app.vue
@@ -21,13 +21,13 @@ useHead({
 
 useSeoMeta({
   title: 'GlowUp Career Consultant',
-  description: 'GlowUp Career Consultant adalah start-up pengembangan karir yang menawarkan layanan lengkap, termasuk pembuatan/review CV, optimasi portofolio, LinkedIn, pelatihan interview, serta career coaching',
+  description: 'Start-up pengembangan karir yang menawarkan layanan pembuatan CV, review CV, optimasi portofolio, optimasi LinkedIn, pelatihan interview, dan bimbingan karier',
   ogTitle: 'GlowUp Career Consultant',
-  ogDescription: 'GlowUp Career Consultant adalah start-up pengembangan karir yang menawarkan layanan lengkap, termasuk pembuatan/review CV, optimasi portofolio, LinkedIn, pelatihan interview, serta career coaching',
+  ogDescription: 'Start-up pengembangan karir yang menawarkan layanan pembuatan CV, review CV, optimasi portofolio, optimasi LinkedIn, pelatihan interview, dan bimbingan karier',
   ogImage: `${APP_URL}/assets/images/about-us.webp`,
   ogUrl: APP_URL,
   twitterTitle: 'GlowUp Career Consultant',
-  twitterDescription: 'GlowUp Career Consultant adalah start-up pengembangan karir yang menawarkan layanan lengkap, termasuk pembuatan/review CV, optimasi portofolio, LinkedIn, pelatihan interview, serta career coaching',
+  twitterDescription: 'Start-up pengembangan karir yang menawarkan layanan pembuatan CV, review CV, optimasi portofolio, optimasi LinkedIn, pelatihan interview, dan bimbingan karier',
   twitterImage: `${APP_URL}/assets/images/about-us.webp`,
   twitterCard: 'summary_large_image'
 })
