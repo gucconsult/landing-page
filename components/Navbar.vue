5c4bae7190bec58277b528542fd7001afe2f7617
RanuDewantara <ranuputradewantara@programmer.net>
2025-04-23 21:04:58 +0700

    feat: Visi-Misii & fix:logo and icon & add: hover

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="bg-white/85 backdrop-blur-sm w-full fixed top-0 left-0 z-50 border-b border-gray-200">
-    <nav class="max-w-7xl mx-auto p-4">
+  <header class="bg-white/50 backdrop-blur-3xl w-full fixed top-0 left-0 z-50 border-b border-gray-100">
+    <nav class="max-w-7xl mx-auto p-2">
       <div class="container mx-auto flex justify-between items-center">
         <ApplicationName />
         <div class="flex gap-6 items-center">
